925a573bb50f2fb6f9f3bfc0c14d3c419218a407
James Ottaway <james@ottaway.mp>
2009-11-21 17:11:10 +1100

    Finished first revision of User model with Cucumber scenario

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
 
 def new
+  @user = User.new
+end
+
+def create
+  @user = User.new(params[:user])
+  
+  if @user.save
+    redirect_to user_path(@user)
+    flash[:notice] = "Successfully Registered User"
+  end
+end
+
+def show
+  @user = User.find(params[:id])
 end
 
 end
